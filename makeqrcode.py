390507422519fc132a86dbc1a6c8b329979341ff
Felix <smslogo@github.com>
2025-08-06 02:08:46 +0200

    add QR

---
+++ makeqrcode.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+
+from segno import helpers
+
+card = {
+    'name' : "Ulrike,Martin",
+    'email':"test@mail.com",
+    'url' :"https://sms-2025.pages.dev/",
+}
+output_filename = "images/qr.png"
+output_option = {
+    'dark': "black",
+    'light': "#8DBEF1FF",
+    'border': 5,
+    'scale': 5
+}
+
+qrcode = helpers.make_mecard(**card)
+qrcode.save(output_filename, **output_option)
+
